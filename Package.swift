7d0c09622694f82fd219b918ffd5116d05f9e47d
Gabriel Alexandru <gabriel.alexandru@cst.ro>
2025-08-28 14:18:04 +0300

    v1.0.0

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+let package = Package(
+    name: "biqSdk",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "biqSdk",
+            targets: ["biqSdk"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "biqSdk",
+            url: "https://github.com/biqProtocol/biq-sdk-ios-repo/biqSdk.xcframework.zip",
+            checksum: "2181cdbcba466ec46ea5677155cf5b64136dbfd6f8c042a58f10c253bda2a295"
+        )
+    ]
+)
+
