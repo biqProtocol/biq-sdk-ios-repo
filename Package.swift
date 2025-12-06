1c65a8fb8ab8eb90efb7f50a3f5d5534f3c752f2
Gabriel Alexandru <gabriel.alexandru.93@gmail.com>
2025-12-06 17:48:48 +0200

    v1.0.17

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "biqSdk",
             url: "https://biqprotocol.github.io/biq-sdk-ios-repo/biqSdk.xcframework.zip",
-            checksum: "1601c789c91a9a2959cf34fc07c1e5806d443d9bc194333f7948852a176873e2"
+            checksum: "125aef18fcbc702ce4d0caac7d10b156aaf00560bdf0f29812f7e87c6896470c"
         )
     ]
 )
